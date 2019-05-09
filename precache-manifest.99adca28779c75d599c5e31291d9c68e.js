46db3abc9010c399087c2422220e3708c19439e3
panzerstadt <tliqun@gmail.com>
2019-05-09 22:36:16 +0900

    Updates

---
+++ precache-manifest.99adca28779c75d599c5e31291d9c68e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "377c30b15a1ad4b1feea6cad24f8a749",
+    "url": "/tut-reactdnd-knight/index.html"
+  },
+  {
+    "revision": "8ce7ec47b5c513d9e12b",
+    "url": "/tut-reactdnd-knight/static/css/main.dba7072e.chunk.css"
+  },
+  {
+    "revision": "9b893c9d877fefc10a0a",
+    "url": "/tut-reactdnd-knight/static/js/2.195ad2d1.chunk.js"
+  },
+  {
+    "revision": "8ce7ec47b5c513d9e12b",
+    "url": "/tut-reactdnd-knight/static/js/main.c4accd8f.chunk.js"
+  },
+  {
+    "revision": "e6e54b5ec87e085239dd",
+    "url": "/tut-reactdnd-knight/static/js/runtime~main.8a517288.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tut-reactdnd-knight/static/media/logo.5d5d9eef.svg"
+  }
+]);
